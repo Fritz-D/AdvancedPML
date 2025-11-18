5fc86a8401b23d2cbd7a8604e632997ddac6ce5e
Fritz-D <104021119+Fritz-D@users.noreply.github.com>
2025-11-18 15:40:59 +0100

    offsetarrays

--- Exercise 2/code as a file because it doesnt fucking work for some reason.jl
+++ Exercise 2/code as a file because it doesnt fucking work for some reason.jl
@@ -14,11 +14,11 @@ f6(d)           = Int(d > 0)
 
 # (a) marginals
 pm(s1,s2,p1,p2,d) = f1(s1) * f2(s1, p1) * f3(s2) * f4(s2, p2) * f5(p1, p2, d) * f6(d)
-pm_s1(s1) = sum(pm(s1,s2,p1,p2,d) for (s2,p1,p2,d)  in Iterators.product(1:N, 1:N, 1:N, -N:N))
-pm_s2(s2) = sum(pm(s1,s2,p1,p2,d) for (s1,p1,p2,d)  in Iterators.product(1:N, 1:N, 1:N, -N:N))
-pm_p1(p1) = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p2,d)  in Iterators.product(1:N, 1:N, 1:N, -N:N))
-pm_p2(p2) = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p1,d)  in Iterators.product(1:N, 1:N, 1:N, -N:N))
-pm_d(d)   = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p1,p2) in Iterators.product(1:N, 1:N, 1:N,  1:N))
+pm_s1(s1) = sum(pm(s1,s2,p1,p2,d) for (s2,p1,p2,d)  in Iterators.product(S2, P1, P2, D))
+pm_s2(s2) = sum(pm(s1,s2,p1,p2,d) for (s1,p1,p2,d)  in Iterators.product(S1, P1, P2, D))
+pm_p1(p1) = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p2,d)  in Iterators.product(S1, S2, P2, D))
+pm_p2(p2) = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p1,d)  in Iterators.product(S1, S2, P1, D))
+pm_d(d)   = sum(pm(s1,s2,p1,p2,d) for (s1,s2,p1,p2) in Iterators.product(S1, S2, P1, P2))
 
 # (b) messages
 # forward pass
@@ -32,13 +32,13 @@ f_p_p1 = m_f2p1
 f_p_p2 = m_f4p2
 m_p1f5 = f_p_p1
 m_p2f5 = f_p_p2
-m_f5d  = [sum(f5(p1, p2, d) * m_p1f5[p1] * m_p2f5[p2] for (p1, p2) in Iterators.product(1:N, 1:N)) for d in D]
-f_p_d  = m_f5d
+m_f5d  = OffsetArray([sum(f5(p1, p2, d) * m_p1f5[p1] * m_p2f5[p2] for (p1, p2) in Iterators.product(1:N, 1:N)) for d in D], D)
+f_p_d  = OffsetArray(m_f5d, D)
 # setting y = 1
 # backward pass
-m_f6d  = f6.(D)
-b_p_d  = m_f5d .* m_f6d # backward pass probs
-m_df5  = b_p_d ./ m_f5d .* Int.(m_f5d .> 0)
+m_f6d  = OffsetArray(f6.(D), D)
+b_p_d  = OffsetArray(m_f5d .* m_f6d, D) # backward pass probs
+m_df5  = OffsetArray(b_p_d ./ m_f5d .* Int.(m_f5d .> 0), D)
 m_f5p1 = [sum(f5(p1,p2,d) * m_df5[d] * m_p2f5[p2] for (p2, d) in Iterators.product(1:N, -N:N)) for p1 in P1]
 m_f5p2 = [sum(f5(p1,p2,d) * m_df5[d] * m_p1f5[p1] for (p1, d) in Iterators.product(1:N, -N:N)) for p2 in P2]
 b_p_p1 = m_f2p1 .* m_f5p1
